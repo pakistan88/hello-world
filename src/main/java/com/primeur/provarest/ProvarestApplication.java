1d100aba02accbcdfe5c63bb9a9b214db6aaa75c
Pasquale Ponzo <pasquale.ponzo@primeur.com>
2018-06-05 17:19:07 +0200

    Aggiunta app

---
+++ src/main/java/com/primeur/provarest/ProvarestApplication.java
@@ -0,0 +1,12 @@
+package com.primeur.provarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvarestApplication.class, args);
+	}
+}
